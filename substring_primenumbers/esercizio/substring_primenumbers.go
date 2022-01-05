8ecf4c15eca188dc6faf3ff21e342fdee856eb64
Marcella Malune <marcella.malune@gmail.com>
2022-01-05 23:10:34 +0100

    substring prime numbers

--- substring_primenumbers/esercizio/substring_primenumbers.go
+++ substring_primenumbers/esercizio/substring_primenumbers.go
@@ -5,14 +5,42 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
 	num := readNumber()
+	length := len(num)
+
+	for i := 0; i < 3; i++ {
+		for j := 0; j < length-i; j++ {
+			newNum := removeDigits(num, i+1, j)
+			if isPrime(newNum) {
+				fmt.Println(newNum)
+			}
+		}
+	}
 }
 
 func readNumber() (input string) {
-	input := os.Args[1]
+	input = os.Args[1]
 	return input
+}
+
+func removeDigits(number string, quantity, position int) (newNumber int) {
+	newString := number[:position] + number[position+quantity:]
+	newNumber, _ = strconv.Atoi(newString)
+	return newNumber
+}
+
+func isPrime(num int) bool {
+	for i := 2; i < num; i++ {
+		if num%i == 0 {
+			return false
+		}
+	}
+
+	return true
 }
